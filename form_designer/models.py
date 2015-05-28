6e87a5b15d08b265b1cf76ec7da172bfbb1ed871
Jason Housley <HousleyJK@gmail.com>
2015-05-28 12:35:17 -0600

    Fix user model dependency.

--- form_designer/models.py
+++ form_designer/models.py
@@ -1,22 +1,13 @@
 import re
-import hashlib, uuid
+import hashlib
+import uuid
 from decimal import Decimal
 
 from django.db import models
-from django.utils.translation import ugettext, ugettext_lazy as _
-from django.forms import widgets
-from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
 from django.conf import settings as django_settings
 from django.utils.datastructures import SortedDict
 from django.core.exceptions import ImproperlyConfigured
-
-# support for custom User models in Django 1.5+
-try:
-    from django.contrib.auth import get_user_model
-except ImportError:  # django < 1.5
-    from django.contrib.auth.models import User
-else:
-    User = get_user_model()
 
 from form_designer.fields import TemplateTextField, TemplateCharField, ModelNameField, RegexpExpressionField
 from form_designer.utils import get_class
@@ -48,13 +39,13 @@ class FormDefinition(models.Model):
     mail_to = TemplateCharField(_('send form data to e-mail address'), help_text=('Separate several addresses with a comma. Your form fields are available as template context. Example: "admin@domain.com, {{ from_email }}" if you have a field named `from_email`.'), max_length=255, blank=True, null=True)
     mail_from = TemplateCharField(_('sender address'), max_length=255, help_text=('Your form fields are available as template context. Example: "{{ first_name }} {{ last_name }} <{{ from_email }}>" if you have fields named `first_name`, `last_name`, `from_email`.'), blank=True, null=True)
     mail_subject = TemplateCharField(_('email subject'), max_length=255, help_text=('Your form fields are available as template context. Example: "Contact form {{ subject }}" if you have a field named `subject`.'), blank=True, null=True)
-    mail_uploaded_files  = models.BooleanField(_('Send uploaded files as email attachments'), default=True)
-    method = models.CharField(_('method'), max_length=10, default="POST", choices = (('POST', 'POST'), ('GET', 'GET')))
+    mail_uploaded_files = models.BooleanField(_('Send uploaded files as email attachments'), default=True)
+    method = models.CharField(_('method'), max_length=10, default="POST", choices=(('POST', 'POST'), ('GET', 'GET')))
     success_message = models.CharField(_('success message'), max_length=255, blank=True, null=True)
     error_message = models.CharField(_('error message'), max_length=255, blank=True, null=True)
     submit_label = models.CharField(_('submit button label'), max_length=255, blank=True, null=True)
     log_data = models.BooleanField(_('log form data'), help_text=_('Logs all form submissions to the database.'), default=True)
-    save_uploaded_files  = models.BooleanField(_('save uploaded files'), help_text=_('Saves all uploaded files using server storage.'), default=True)
+    save_uploaded_files = models.BooleanField(_('save uploaded files'), help_text=_('Saves all uploaded files using server storage.'), default=True)
     success_redirect = models.BooleanField(_('HTTP redirect after successful submission'), default=True)
     success_clear = models.BooleanField(_('clear form after successful submission'), default=True)
     allow_get_initial = models.BooleanField(_('allow initial values via URL'), help_text=_('If enabled, you can fill in form fields by adding them to the query string.'), default=True)
@@ -75,7 +66,6 @@ class FormDefinition(models.Model):
 
     def get_field_dict(self):
         field_dict = SortedDict()
-        names = []
         for field in self.formdefinitionfield_set.all():
             field_dict[field.name] = field
         return field_dict
@@ -176,7 +166,7 @@ class FormDefinition(models.Model):
     @property
     def submit_flag_name(self):
         name = settings.SUBMIT_FLAG_NAME % self.name
-        # make sure we are not overriding one of the actual form fields 
+        # make sure we are not overriding one of the actual form fields
         while self.formdefinitionfield_set.filter(name__exact=name).count() > 0:
             name += '_'
         return name
@@ -218,14 +208,14 @@ class FormDefinitionField(models.Model):
         ordering = ['position']
 
     def save(self, *args, **kwargs):
-        if self.position == None:
+        if self.position is None:
             self.position = 0
         super(FormDefinitionField, self).save(*args, **kwargs)
 
     def ____init__(self, field_class=None, name=None, required=None, widget=None, label=None, initial=None, help_text=None, *args, **kwargs):
         super(FormDefinitionField, self).__init__(*args, **kwargs)
         self.name = name
-        self.field_class = field_class  
+        self.field_class = field_class
         self.required = required
         self.widget = widget
         self.label = label
@@ -248,14 +238,14 @@ class FormDefinitionField(models.Model):
 
         if self.field_class in ('django.forms.IntegerField', 'django.forms.DecimalField'):
             args.update({
-                'max_value': int(self.max_value) if self.max_value != None else None,
-                'min_value': int(self.min_value) if self.min_value != None else None,
+                'max_value': int(self.max_value) if self.max_value is not None else None,
+                'min_value': int(self.min_value) if self.min_value is not None else None,
             })
 
         if self.field_class == 'django.forms.DecimalField':
             args.update({
-                'max_value': Decimal(str(self.max_value)) if self.max_value != None else None,
-                'min_value': Decimal(str(self.min_value)) if self.max_value != None else None,
+                'max_value': Decimal(str(self.max_value)) if self.max_value is not None else None,
+                'min_value': Decimal(str(self.min_value)) if self.max_value is not None else None,
                 'max_digits': self.max_digits,
                 'decimal_places': self.decimal_places,
             })
@@ -306,12 +296,11 @@ class FormDefinitionField(models.Model):
 class FormLog(models.Model):
     form_definition = models.ForeignKey(FormDefinition, related_name='logs')
     created = models.DateTimeField(_('Created'), auto_now=True)
-    created_by = models.ForeignKey(User, null=True, blank=True)
+    created_by = models.ForeignKey(django_settings.AUTH_USER_MODEL, null=True, blank=True)
     _data = None
 
     def __unicode__(self):
-        return "%s (%s)" % (self.form_definition.title or  \
-            self.form_definition.name, self.created) 
+        return "%s (%s)" % (self.form_definition.title or self.form_definition.name, self.created)
 
     def get_data(self):
         if self._data:
@@ -330,8 +319,7 @@ class FormLog(models.Model):
                 # field may have been removed
                 label = None
 
-            value_dict = FormValueDict(item.field_name, item.value,
-                label)
+            value_dict = FormValueDict(item.field_name, item.value, label)
 
             if item.field_name in fields:
                 values_with_header[item.field_name] = value_dict
@@ -357,7 +345,7 @@ class FormLog(models.Model):
 
     def save(self, *args, **kwargs):
         super(FormLog, self).save(*args, **kwargs)
-        if self._data: 
+        if self._data:
             # safe form data and then clear temporary variable
             for value in self.values.all():
                 value.delete()
@@ -378,7 +366,7 @@ class FormValue(models.Model):
         value = PickledObjectField(_('value'), null=True, blank=True)
     else:
         # otherwise just use a TextField, with the drawback that
-        # all values will just be stored as unicode strings, 
+        # all values will just be stored as unicode strings,
         # but you can easily query the database for form results.
         value = models.TextField(_('value'), null=True, blank=True)
 
@@ -389,5 +377,3 @@ class FormValue(models.Model):
 if 'south' in django_settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([], ["^form_designer\.fields\..*"])
-
-
